4e317ea689d7819a672b1f4cbf17d6972d97f038
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2024-11-11 23:22:24 +0200

    lab5

---
+++ lab5/LR_5_task_3.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from utilities import visualize_classifier
+
+input_file = 'data_random_forests.txt'
+data = np.loadtxt(input_file, delimiter=',')
+X, y = data[:, :-1], data[:, -1]
+
+class_0 = np.array(X[y==0])
+class_1 = np.array(X[y==1])
+class_2 = np.array(X[y==2])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=5)
+
+# Визначення сітки значень параметрів
+parameter_grid = [
+    {'n_estimators': [100], 'max_depth': [2, 4, 7, 12, 16]},
+    {'max_depth': [4], 'n_estimators': [25, 50, 100, 250]}
+]
+
+metrics = ['precision_weighted', 'recall_weighted']
+for metric in metrics:
+    print("\n##### Searching optimal parameters for", metric)
+
+    classifier = GridSearchCV(
+        ExtraTreesClassifier(random_state=0),
+        parameter_grid, cv=5, scoring=metric)
+    classifier.fit(X_train, y_train)
+
+    print("\nGrid scores for the parameter grid:")
+    means = classifier.cv_results_['mean_test_score']
+    params = classifier.cv_results_['params']
+    for mean, param in zip(means, params):
+        print(param, '-->', round(mean, 3))
+    print("\nBest parameters:", classifier.best_params_)
+
+y_pred = classifier.predict(X_test)
+print("\nPerformance report:\n")
+print(classification_report(y_test, y_pred))
